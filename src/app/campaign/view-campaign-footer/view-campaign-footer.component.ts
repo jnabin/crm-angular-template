e0da95d5cc524bb40696a7d37926ab20757e0402
nabin <jahangirnabin2@gmail.com>
2022-05-19 01:14:25 +0600

    connect button with loader

--- src/app/campaign/view-campaign-footer/view-campaign-footer.component.ts
+++ src/app/campaign/view-campaign-footer/view-campaign-footer.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-campaign-footer.component.scss']
 })
 export class ViewCampaignFooterComponent implements OnInit {
-
+  loading: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  connect() {
+    this.loading = true;
+    setTimeout(this.hide, 2000);
+  }
+
+  hide = () =>{
+    this.loading = false
+  };
 
 }
