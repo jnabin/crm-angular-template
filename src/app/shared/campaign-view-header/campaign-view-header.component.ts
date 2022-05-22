7123dd112bff483506878c558e1e63219bcf795a
nabin <jahangirnabin2@gmail.com>
2022-05-22 20:06:23 +0600

    minor improvement

--- src/app/shared/campaign-view-header/campaign-view-header.component.ts
+++ src/app/shared/campaign-view-header/campaign-view-header.component.ts
@@ -1,28 +1,36 @@
+import { Subscription } from 'rxjs/internal/Subscription';
 import { CampaignService, ICallConnection } from './../../campaign/services/campaign.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-campaign-view-header',
   templateUrl: './campaign-view-header.component.html',
   styleUrls: ['./campaign-view-header.component.scss']
 })
-export class CampaignViewHeaderComponent implements OnInit {
+export class CampaignViewHeaderComponent implements OnInit, OnDestroy {
   campaignName: string = "Campaign1";
   showProgress: boolean = false;
   eta: number = 49;
   completionPercent: number = 0;
+  subscription?: Subscription;
+  subscription1?: Subscription;
 
   constructor(private CampaignService: CampaignService) { }
 
   ngOnInit(): void {
-    this.CampaignService.getshowProgress.subscribe((result: boolean) => {
+    this.subscription = this.CampaignService.getshowProgress.subscribe((result: boolean) => {
       this.showProgress = result;
     });
 
-    this.CampaignService.getconnectionStatus.subscribe((result: ICallConnection) => {
+    this.subscription1 = this.CampaignService.getconnectionStatus.subscribe((result: ICallConnection) => {
       this.eta = result.count;
       this.completionPercent = result.progressPercentage;
     });
+  }
+
+  ngOnDestroy(): void {
+    if(this.subscription) this.subscription.unsubscribe()
+    if(this.subscription1) this.subscription1.unsubscribe()
   }
 
 }
